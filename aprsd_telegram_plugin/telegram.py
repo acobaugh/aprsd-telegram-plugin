a672c613accd6d7f42ff18e0fbe60ef09966cdc7
Andy Cobaugh <andrew.cobaugh@gmail.com>
2024-06-17 01:57:25 -0400

    wip

--- aprsd_telegram_plugin/telegram.py
+++ aprsd_telegram_plugin/telegram.py
@@ -109,6 +109,7 @@ class TelegramChatPlugin(plugin.APRSDRegexCommandPluginBase):
             self.application = Application.builder().token(token).build()
             _loop.run_until_complete(self.application.updater.initialize())
             _loop.run_until_complete(self.application.updater.start_polling())
+            LOG.info(f"Started telegram bot: {self.bot.name}")
         except Exception as ex:
             self.enabled = False
             LOG.exception(ex)
